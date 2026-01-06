40ae68a509bee8bd9ccf5d07f29c6fe9f504d75a
krishn-cti <krishn.ctinfotech@gmail.com>
2026-01-06 16:17:13 +0530

    changes on the base path

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,19 +26,7 @@ import {
 dotenv.config();
 const __dirname = path.resolve();
 
-const getLocalIp = () => {
-    const interfaces = os.networkInterfaces();
-    for (const name of Object.keys(interfaces)) {
-        for (const net of interfaces[name]) {
-            if (net.family === "IPv4" && !net.internal) {
-                return net.address;
-            }
-        }
-    }
-    return "localhost";
-};
-
-let localIp = getLocalIp();
+let localIp = process.env.LOCAL_IP;
 let baseUrl = `http://${localIp}:${process.env.PORT}`;
 
 // Login API
--- controllers/userController.js
+++ controllers/userController.js
@@ -20,19 +20,7 @@ import {
 
 const __dirname = path.resolve();
 
-const getLocalIp = () => {
-    const interfaces = os.networkInterfaces();
-    for (const name of Object.keys(interfaces)) {
-        for (const net of interfaces[name]) {
-            if (net.family === "IPv4" && !net.internal) {
-                return net.address;
-            }
-        }
-    }
-    return "localhost";
-};
-
-let localIp = getLocalIp();
+let localIp = process.env.LOCAL_IP;
 let baseUrl = `http://${localIp}:${process.env.PORT}`;
 
 // function to generate random alphanumeric string
